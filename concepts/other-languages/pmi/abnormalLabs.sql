e468fc050abdc3445748d3dd4bd8a56b86b8c656
Rob Challen <rob@terminological.co.uk>
2017-10-12 15:59:33 +0100

    update pmi to build incrementally

--- concepts/other-languages/pmi/abnormalLabs.sql
+++ concepts/other-languages/pmi/abnormalLabs.sql
@@ -2,6 +2,8 @@
 
 DROP TABLE IF exists D_LABMEAN;
 
+-- generate a table with the mean and std of the results of a test
+-- on a gender specific and age range specific way
 CREATE TABLE D_LABMEAN as
 (
 SELECT
@@ -21,21 +23,44 @@ GROUP BY e.ITEMID, e.VALUEUOM, FLOOR(DATEDIFF(e.CHARTTIME,p.DOB)/365.25/5)*5, p.
 
 ALTER TABLE D_LABMEAN add index (ITEMID,VALUEUOM,ageCat,GENDER);
 
-SELECT 
-e.*,
-IF( e.VALUENUM < l.meanValue , "low","high") as indicator,
-(e.VALUENUM - l.meanValue)/l.stdValue as stdevsFromMean
-FROM MIMIC.LABEVENTS e, D_LABMEAN l, PATIENTS p
+ALTER TABLE LABEVENTS 
+ADD COLUMN INDICATOR char DEFAULT NULL,
+ADD COLUMN DEGREE float DEFAULT NULL;
+
+UPDATE LABEVENTS e, D_LABMEAN l, PATIENTS p
+SET e.INDICATOR=IF(e.VALUENUM<l.meanValue, "L","H")
 WHERE e.ITEMID = l.ITEMID
 AND e.VALUEUOM = l.VALUEUOM
 AND e.FLAG = "abnormal"
 AND p.SUBJECT_ID = e.SUBJECT_ID
 AND p.GENDER = l.GENDER
-AND FLOOR(DATEDIFF(e.CHARTTIME,p.DOB)/365.25/5)*5 = l.ageCat
-;
+AND FLOOR(DATEDIFF(e.CHARTTIME,p.DOB)/365.25/5)*5 = l.ageCat;
 
-DROP TABLE IF exists D_LABREFRANGE;
+UPDATE LABEVENTS e, D_LABMEAN l, PATIENTS p
+SET e.DEGREE=(e.VALUENUM - l.meanValue)/l.stdValue
+WHERE e.ITEMID = l.ITEMID
+AND e.VALUEUOM = l.VALUEUOM
+AND p.SUBJECT_ID = e.SUBJECT_ID
+AND p.GENDER = l.GENDER
+AND l.stdValue>0
+AND FLOOR(DATEDIFF(e.CHARTTIME,p.DOB)/365.25/5)*5 = l.ageCat;
 
+-- SELECT 
+-- e.*,
+-- IF( e.VALUENUM < l.meanValue , "low","high") as indicator,
+-- (e.VALUENUM - l.meanValue)/l.stdValue as stdevsFromMean
+-- FROM MIMIC.LABEVENTS e, D_LABMEAN l, PATIENTS p
+-- WHERE e.ITEMID = l.ITEMID
+-- AND e.VALUEUOM = l.VALUEUOM
+-- AND e.FLAG = "abnormal"
+-- AND p.SUBJECT_ID = e.SUBJECT_ID
+-- AND p.GENDER = l.GENDER
+-- AND FLOOR(DATEDIFF(e.CHARTTIME,p.DOB)/365.25/5)*5 = l.ageCat
+-- ;
+
+
+-- The idea here was to find the smallest abnormal result above the mean of the results 
+-- grouped by gender and age band to determine the highest limit of normal
 -- This doesn't really work as there is not enough data and patients are all abnormal.
 SELECT 
 MAX(e.VALUENUM) as lowerLimitNormal,
